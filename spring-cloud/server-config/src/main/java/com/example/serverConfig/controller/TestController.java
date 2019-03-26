c7c0a4b2804cf7021bd904fe1cedbc2b259cdcd8
mikasa007 <17863109506@163.com>
2019-03-26 17:05:45 +0800

    测试一下

---
+++ spring-cloud/server-config/src/main/java/com/example/serverConfig/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.example.serverConfig.controller;
+
+/**
+ * @ClassName TestController
+ * @description
+ * @author: tianluhua
+ * @date 2019/3/26 17:05
+ */
+public class TestController {
+}
